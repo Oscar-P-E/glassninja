036b45a8e378dfcd1559872af1fa97d6ece3e574
Oscar El <opelm1@gmail.com>
2023-11-14 14:47:41 +1000

    Complete draft of expanding square version

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,16 @@
 
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
-import { WhyUs } from "./components/WhyUs";
+// import { WhyUs } from "./components/WhyUs";
 
 const App = () => {
   return (
     <>
-      {/* <div className="min-h-screen"> */}
-      <Header />
-      <Hero />
-      {/* </div> */}
-      <WhyUs />
+      <div className="flex flex-col h-screen">
+        <Header />
+        <Hero />
+      </div>
+      {/* <WhyUs /> */}
       {/* <About /> */}
       {/* <Services /> */}
       {/* <Contact /> */}
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,135 @@
-// Hero.tsx
+import { MouseEvent, useState } from "react";
+import backgroundImage from "/img/pexels-window-bg.jpg";
 
 export const Hero = () => {
+  const [activeCard, setActiveCard] = useState<number | null>(null);
+  const [transformOrigin, setTransformOrigin] = useState({ x: 0, y: 0 });
+
+  const cardContents = [
+    {
+      title: "Window Cleaning",
+      description: "Servicing the Sunshine Coast and beyond.",
+    },
+    {
+      title: "Why Us?",
+      description: "Professional, Reliable, Insured",
+    },
+    {
+      title: "Service Areas",
+      description: "Beerwah, Caloundra, Mooloolaba, Nambour, Maleny...",
+    },
+    {
+      title: "Contact Us",
+      description: "This is a contact form.",
+    },
+  ];
+
+  const handleCardClick = (
+    index: number,
+    event: MouseEvent<HTMLDivElement>
+  ) => {
+    const mouseX = event.clientX;
+    const mouseY = event.clientY;
+    setTransformOrigin({ x: mouseX, y: mouseY });
+    setActiveCard(activeCard === index ? null : index);
+  };
+
   return (
-    <div className="p-2 pb-0">
-      <div className="border border-black p-8">
-        <h1 className="text-6xl leading-none pb-6">Window Cleaning</h1>
-        <p className="text-xl pb-7">
-          Professional window cleaning services on the Sunshine Coast and
-          surrounding areas.
-        </p>
-        <div className="bg-black w-fit h-fit p-1">
-        <button className="text-xl bg-black text-white w-fit ml-auto border border-white leading-none px-4 py-2">
-          Get A Quote
-        </button>
+    <>
+      <style>
+        {`
+          @keyframes grow {
+            from {
+              transform: scale(0.1);
+            }
+            to {
+              transform: scale(1);
+            }
+          }
+
+          @keyframes contentFadeIn {
+            from {
+              transform: scale(0.5);
+              opacity: 0;
+            }
+            to {
+              transform: scale(1);
+              opacity: 1;
+            }
+          }
+
+          @media (min-width: 768px) {
+            .expanded-card {
+              padding: 2rem;
+              background-color: rgba(255, 255, 255, 1);
+            }
+          }
+        `}
+      </style>
+
+      <div
+        className="flex-1 bg-black flex justify-center items-center overflow-hidden"
+        style={{
+          backgroundImage: `url(${backgroundImage})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <div className="grid grid-cols-2 gap-4 p-2 sm:p-4">
+          {/* Cards */}
+          {cardContents.map((card, index) => (
+            <div
+              key={index}
+              className="bg-white bg-opacity-80 aspect-square h-36 sm:h-64 p-4 flex flex-col justify-between transition duration-500 ease-in-out"
+              onClick={(e) => handleCardClick(index, e)}
+              style={{ cursor: "pointer", transformOrigin: "center center" }}
+            >
+              <div
+                style={{ animation: "contentFadeIn 0.5s ease-out forwards" }}
+              >
+                <h1 className="text-2xl  sm:text-4xl leading-none pb-4">
+                  {card.title}
+                </h1>
+                <p className="text-xs sm:text-lg pb-4">{card.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
+
+        {/* Overlay for Expanded Card */}
+        {activeCard !== null && (
+          <div
+            className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center"
+            onClick={() => setActiveCard(null)}
+          >
+            <div
+              className={`bg-white p-4 aspect-square ${
+                activeCard !== null ? "expanded-card" : ""
+              }`}
+              style={{
+                width: "90vw",
+                height: "90vh",
+                transformOrigin: `${transformOrigin.x}px ${transformOrigin.y}px`,
+                animation: "grow 0.5s ease-out forwards",
+              }}
+            >
+              <div
+                style={{ animation: "contentFadeIn 0.5s ease-out forwards" }}
+              >
+                <h1 className="text-2xl sm:text-3xl md:text-4xl leading-none pb-4">
+                  {cardContents[activeCard].title}
+                </h1>
+                <p className="text-sm sm:text-base md:text-lg pb-4">
+                  {cardContents[activeCard].description}
+                </p>
+
+              </div>
+            </div>
+          </div>
+        )}
       </div>
-    </div>
+    </>
   );
 };
+
+export default Hero;
